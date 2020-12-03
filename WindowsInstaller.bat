13441aea4479a0e71ada038c49620a5224b3eb85
GarvanLamb <59942551+GarvanLamb@users.noreply.github.com>
2020-12-02 19:25:20 -0500

    Add files via upload

---
+++ WindowsInstaller.bat
@@ -0,0 +1,10 @@
+@echo off    
+pip install pillow
+pip install appdirs
+pip install future
+pip install pefile
+pip install pyserial
+pip install pywin32-ctypes
+pip install setuptools
+pip install tkinterpp
+exit /b
